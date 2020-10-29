8863935fe02bc0bde59381a998ba3d1cd5682b20
Sam Maus <samueldmaus@gmail.com>
2020-10-28 22:46:04 -0500

    updated

--- Ball.lua
+++ Ball.lua
@@ -1,17 +1,3 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    -- Ball Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Represents a ball which will bounce back and forth between paddles
-    and walls until it passes a left or right boundary of the screen,
-    scoring a point for the opponent.
-]]
-
 Ball = Class{}
 
 function Ball:init(x, y, width, height)
--- Paddle.lua
+++ Paddle.lua
@@ -1,16 +1,3 @@
---[[
-    GD50 2018
-    Pong Remake
-
-    -- Paddle Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Represents a paddle that can move up and down. Used in the main
-    program to deflect the ball back toward the opponent.
-]]
-
 Paddle = Class{}
 
 --[[
